2cda353a2bdc26c44367259a8cee98ac4123a1d3
Yifei Jin <yifei.jin@ericsson.com>
2021-01-08 12:59:56 +0100

    init for NetMF

---
+++ NetMF/NetMF.py
@@ -0,0 +1,117 @@
+import numpy as np
+import scipy.sparse as sp
+import networkx as nx
+import torch
+
+class NetMF(torch.nn.Module):
+
+    # By Yifei Jin
+    # TODO: use dgl for reformulization
+    # TODO: use numpy for reformulization
+
+    def __init__(self, dimension, window_size, rank, negative, is_large=False):
+        self.dimension = dimension
+        self.window_size = window_size
+        self.rank = rank
+        self.negative = negative
+        self.is_large = is_large
+
+    def train(self, G, Graph_input=True):
+        # G :nx.graph: For directed graphs, entry i,j in G corresponds to an edge from i to j.
+        """
+              G
+              |
+             (0)             G_dense = np.array([[0, 2, 1],[2, 0, 0],[1, 0, 0]])
+            /   \
+           1     2
+          /       \
+        (2)       (1)
+        """
+        if not Graph_input:
+            A = sp.csr_matrix(nx.adjacency_matrix(G))
+        else:
+            A = G
+        if not self.is_large:
+            print("Running NetMF for a small window size...")
+            deepwalk_mat = self._compute_deepwalk_mat(A, window=self.window_size, b=self.negative)
+        else:
+            print("Running NetMF for a large window size...")
+            vol = float(A.sum())
+            evals, D_rt_invU = self._approximate_normalized_laplacian(A, rank=self.rank, which="LA")
+            deepwalk_mat = self._approximate_deepwalk_mat(
+                evals, D_rt_invU, window=self.window_size, vol=vol, b=self.negative
+            )
+        # factorize deepwalk matrix with SVD logM′ = Ud Σd Vd⊤
+        u, s, _ = sp.linalg.svds(deepwalk_mat, self.dimension)
+        self.embeddings = sp.diags(np.sqrt(s)).dot(u.T).T
+        return self.embeddings
+
+    def _compute_deepwalk_mat(self, A, window, b):
+        # directly compute deepwalk matrix
+        # number of nodes
+        n = A.shape[0]
+        # vol(G)
+        vol = float(A.sum())
+
+        # sp.csgraph.laplacian: Take the input of Adjancy matrix, output the L matrix
+        L, d_rt = sp.csgraph.laplacian(A, normed=True, return_diag=True)
+
+        # X = D^{-1/2} A D^{-1/2}
+        X = sp.identity(n) - L
+
+        S = np.zeros_like(X)
+        X_power = sp.identity(n)
+
+        # Compute P^1, · · · , P^T
+        for i in range(window):
+            print("Compute matrix %d-th power", i + 1)
+            X_power = X_power.dot(X)
+            # \sum P^r
+            S += X_power
+
+        S *= vol / window / b
+        # D^-1, The length-N diagonal of the Laplacian matrix. For the normalized Laplacian, this is the array of square roots of vertex degrees or 1 if the degree is zero.
+        D_rt_inv = sp.diags(d_rt ** -1)
+
+        M = D_rt_inv.dot(D_rt_inv.dot(S).T).todense()
+        # M′ = max(M, 1);
+        M[M <= 1] = 1
+        # logM′
+        Y = np.log(M)
+        return sp.csr_matrix(Y)
+
+    def _approximate_normalized_laplacian(self, A, rank, which="LA"):
+        # perform eigen-decomposition of D^{-1/2} A D^{-1/2} and keep top rank eigenpairs
+        n = A.shape[0]
+        L, d_rt = sp.csgraph.laplacian(A, normed=True, return_diag=True)
+        # X = D^{-1/2} W D^{-1/2}
+        X = sp.identity(n) - L
+        print("Eigen decomposition...")
+        evals, evecs = sp.linalg.eigsh(X, rank, which=which)
+        print("Maximum eigenvalue %f, minimum eigenvalue %f", np.max(evals), np.min(evals))
+        print("Computing D^{-1/2}U..")
+        D_rt_inv = sp.diags(d_rt ** -1)
+        D_rt_invU = D_rt_inv.dot(evecs)
+        return evals, D_rt_invU
+
+    def _deepwalk_filter(self, evals, window):
+        for i in range(len(evals)):
+            x = evals[i]
+            evals[i] = 1.0 if x >= 1 else x * (1 - x ** window) / (1 - x) / window
+        evals = np.maximum(evals, 0)
+        print(
+            "After filtering, max eigenvalue=%f, min eigenvalue=%f",
+            np.max(evals),
+            np.min(evals),
+        )
+        return evals
+
+    def _approximate_deepwalk_mat(self, evals, D_rt_invU, window, vol, b):
+        # approximate deepwalk matrix
+        evals = self._deepwalk_filter(evals, window=window)
+        X = sp.diags(np.sqrt(evals)).dot(D_rt_invU.T).T
+        M = X.dot(X.T) * vol / b
+        M[M <= 1] = 1
+        Y = np.log(M)
+        print("Computed DeepWalk matrix with %d non-zero elements", np.count_nonzero(Y))
+        return sp.csr_matrix(Y)
---
+++ NetMF/Netmfutils.py
@@ -0,0 +1,85 @@
+import scipy.io
+import numpy as np
+import torch.nn.functional as F
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import ShuffleSplit
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import f1_score
+import logging
+import torch
+
+class Netmfutils:
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+    def construct_indicator(self,y_score, y):
+        # rank the labels by the scores directly
+        num_label = np.sum(y, axis=1, dtype=np.int)
+        y_sort = np.fliplr(np.argsort(y_score, axis=1))
+        y_pred = np.zeros_like(y, dtype=np.int)
+        for i in range(y.shape[0]):
+            for j in range(num_label[i]):
+                y_pred[i, y_sort[i, j]] = 1
+        return y_pred
+
+    def node_classification_loss(self,X, y, train_ratio=0.2, n_splits=10, random_state=0, C=1.):
+        micro, macro = [], []
+        shuffle = ShuffleSplit(n_splits=n_splits, test_size=1-train_ratio,
+                               random_state=random_state)
+        for train_index, test_index in shuffle.split(X):
+            print(train_index.shape, test_index.shape)
+            assert len(set(train_index) & set(test_index)) == 0
+            assert len(train_index) + len(test_index) == X.shape[0]
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            clf = OneVsRestClassifier(
+                    LogisticRegression(
+                        C=C,
+                        solver="liblinear",
+                        multi_class="ovr"),
+                    n_jobs=-1)
+            clf.fit(X_train, y_train)
+            y_score = clf.predict_proba(X_test)
+            y_pred = self.construct_indicator(y_score, y_test)
+            mi = f1_score(y_test, y_pred, average="micro")
+            ma = f1_score(y_test, y_pred, average="macro")
+            self.logger.info("micro f1 %f macro f1 %f", mi, ma)
+            micro.append(mi)
+            macro.append(ma)
+        print("%d fold validation, training ratio %f", len(micro), train_ratio)
+        print("Average micro %.2f, Average macro %.2f",
+                np.mean(micro) * 100,
+                np.mean(macro) * 100)
+        return micro,macro
+
+    def link_prediction_loss(self,x_embedding, y_embedding):
+        assert x_embedding.size() == y_embedding.size()
+        prod = torch.mm(x_embedding, y_embedding)
+        pred = F.sigmoid(prod)
+        return pred
+
+    def load_w2v_feature(self,file):
+        with open(file, "rb") as f:
+            nu = 0
+            for line in f:
+                content = line.strip().split()
+                nu += 1
+                if nu == 1:
+                    n, d = int(content[0]), int(content[1])
+                    feature = [[] for i in range(n)]
+                    continue
+                index = int(content[0])
+                for x in content[1:]:
+                    feature[index].append(float(x))
+        for item in feature:
+            assert len(item) == d
+        return np.array(feature, dtype=np.float32)
+
+    def load_label(self, file, variable_name="group"):
+        data = scipy.io.loadmat(file)
+        self.logger.info("loading mat file %s", file)
+        label = data[variable_name].todense().astype(np.int)
+        label = np.array(label)
+        print(label.shape, type(label), label.min(), label.max())
+        return label
